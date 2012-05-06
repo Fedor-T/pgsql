9ff921d2fa924139c58f39b3cdb45e930574d0d1
Fedya <tarasovf@tut.by>
2012-05-06 21:36:13 +0300

    fix configurator

--- pgsql/configuration.h
+++ pgsql/configuration.h
@@ -8,23 +8,21 @@
 // Notice that the configuration file format does not permit values to span
 // more than one line, commentary at the end of a line, or [section]s.
 //
-#ifndef CONFIGURATION_H
-#define CONFIGURATION_H
-
+#pragma once
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <map>
 
 namespace configuration
 {
 	struct data: std::map <std::string, std::string>
-		{
+	{
 		// Here is a little convenience method...
 		bool iskey( const std::string& s ) const
-			{
+		{
 			return count( s ) != 0;
-			}
-		};
+		}
+	};
 
 	//---------------------------------------------------------------------------
 	// The extraction operator reads configuration::data until EOF.
@@ -32,40 +30,40 @@ namespace configuration
 	//
 	std::istream& operator >> ( std::istream& ins, data& d )
 	{
-	std::string s, key, value;
+		std::string s, key, value;
 
-	// For each (key, value) pair in the file
-	while (std::getline( ins, s ))
+		// For each (key, value) pair in the file
+		while (std::getline( ins, s ))
 		{
-		std::string::size_type begin = s.find_first_not_of( " \f\t\v" );
+			std::string::size_type begin = s.find_first_not_of( " \f\t\v" );
 
-		// Skip blank lines
-		if (begin == std::string::npos) continue;
+			// Skip blank lines
+			if (begin == std::string::npos) continue;
 
-		// Skip commentary
-		if (std::string( "#;" ).find( s[ begin ] ) != std::string::npos) continue;
+			// Skip commentary
+			if (std::string( "#;" ).find( s[ begin ] ) != std::string::npos) continue;
 
-		// Extract the key value
-		std::string::size_type end = s.find( '=', begin );
-		key = s.substr( begin, end - begin );
+			// Extract the key value
+			std::string::size_type end = s.find( '=', begin );
+			key = s.substr( begin, end - begin );
 
-		// (No leading or trailing whitespace allowed)
-		key.erase( key.find_last_not_of( " \f\t\v" ) + 1 );
+			// (No leading or trailing whitespace allowed)
+			key.erase( key.find_last_not_of( " \f\t\v" ) + 1 );
 
-		// No blank keys allowed
-		if (key.empty()) continue;
+			// No blank keys allowed
+			if (key.empty()) continue;
 
-		// Extract the value (no leading or trailing whitespace allowed)
-		begin = s.find_first_not_of( " \f\n\r\t\v", end + 1 );
-		end   = s.find_last_not_of(  " \f\n\r\t\v" ) + 1;
+			// Extract the value (no leading or trailing whitespace allowed)
+			begin = s.find_first_not_of( " \f\n\r\t\v", end + 1 );
+			end   = s.find_last_not_of(  " \f\n\r\t\v" ) + 1;
 
-		value = s.substr( begin, end - begin );
+			value = s.substr( begin, end - begin );
 
-		// Insert the properly extracted (key, value) pair into the map
-		d[ key ] = value;
+			// Insert the properly extracted (key, value) pair into the map
+			d[ key ] = value;
 		}
 
-	return ins;
+		return ins;
 	}
 
 	//---------------------------------------------------------------------------
@@ -73,12 +71,10 @@ namespace configuration
 	//
 	std::ostream& operator << ( std::ostream& outs, const data& d )
 	{
-	data::const_iterator iter;
-	for (iter = d.begin(); iter != d.end(); iter++)
-		outs << iter->first << " = " << iter->second << std::endl;
-	return outs;
+		data::const_iterator iter;
+		for (iter = d.begin(); iter != d.end(); iter++)
+			outs << iter->first << " = " << iter->second << std::endl;
+		return outs;
 	}
 
 } // namespace configuration
-
-#endif
