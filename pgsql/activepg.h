2ee4de53fd8cc5e446beb6901498c403cae5f460
Fedya <tarasovf@tut.by>
2012-05-09 15:10:42 +0300

    add headers for models and common header

---
+++ pgsql/activepg.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#if defined(WIN32) || defined(_WIN32)
+#pragma comment(lib,"postgres.lib")
+#pragma comment(lib,"libecpg.lib")
+#pragma comment(lib,"libpgport.lib")
+#pragma comment(lib,"libpq.lib")
+#endif
+
+#include "pgConnection.h"
+#include "models.h"
--- pgsql/main.cpp
+++ pgsql/main.cpp
@@ -1,13 +1,6 @@
-#if defined(WIN32) || defined(_WIN32)
-#pragma comment(lib,"postgres.lib")
-#pragma comment(lib,"libecpg.lib")
-#pragma comment(lib,"libpgport.lib")
-#pragma comment(lib,"libpq.lib")
-#endif
-
 #include <cstdlib>
-#include "pgConnection.h"
-#include "pgAuthor.h"
+#include "activepg.h"
+
 int main()
 {
 	pgConnection dbConnecion;
