209b0ad20388a211971f0ff4cc8b24027fc64c21
FoboCasteR <fobocaster@TACHIKOMA.(none)>
2012-05-13 17:15:39 +0300

    add print for instances

--- pgsql/Instance.cpp
+++ pgsql/Instance.cpp
@@ -12,10 +12,10 @@ Instance::Instance(data attributes): Model("Instance")
 void Instance::print()
 {
 	if(modelData["id"] == "0") return;
-	cout<<setw(5)<<modelData["id"]<<"|"<<setw(5)<<modelData["book_id"]<<"|"<<endl;
+	cout<<setw(5)<<modelData["id"]<<"|"<<setw(9)<<modelData["book_id"]<<"|"<<setw(19)<<modelData["inventory_number"]<<"|"<<setw(11)<<modelData["is_avail"]<<"|"<<setw(14)<<modelData["position"]<<"|"<<endl;
 }
 
 void Instance::printHeader()
 {
-	cout<<setw(6)<<"id: |"<<setw(5)<<"Book ID: |"<<endl;
+	cout<<setw(6)<<"id: |"<<setw(9)<<"Book ID: |"<<setw(20)<<"Inventory number: |"<<setw(12)<<"is avail: |"<<setw(11)<<"Position: |"<<endl;
 }
