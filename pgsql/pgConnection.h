0e7ab43bc08e3c91adc5533a23e53cb86ac1107d
Fedya <tarasovf@tut.by>
2012-05-13 12:21:06 +0300

    remove trash

--- pgsql/pgConnection.h
+++ pgsql/pgConnection.h
@@ -13,11 +13,6 @@ public:
 	bool connectionEstablished();
 	int initDriver();
 	pgDriver* getDriver();
-	/*void establishConnection(char* dbName, char* user, char* password, char* host = "127.0.0.1", char* port = "5432");
-	int connectionEstablished();
-	void closeConnection();
-	int insertData(char* fname, char*lname, char* oname);
-	void selectAuthors();*/
 private:
 	PGconn *connection;
 	pgDriver* driver;
